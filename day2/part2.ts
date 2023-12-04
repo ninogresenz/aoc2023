6149f4aad2470aa9627ef27c9527cac919f1cf18
Nino Gresenz <nino.gresenz@esome.com>
2023-12-04 15:34:46 +0100

    day2

---
+++ day2/part2.ts
@@ -0,0 +1,49 @@
+import fs from "fs";
+
+function getGames(file: string): string[] {
+  const inputRaw = fs.readFileSync(file, {encoding: 'utf-8'})
+  return inputRaw.split('\n').filter(l => l.length > 0)
+}
+
+type CubeSet = {
+  red: number
+  green: number
+  blue: number
+}
+
+function getCubes(s: string): CubeSet {
+  const cubeSet: CubeSet = {red: 0, green: 0, blue: 0}
+  const rounds = s.split('; ')
+  for (const round of rounds) {
+    for (const cubes of round.split(', ')) {
+      const [num, color] = cubes.split(' ')
+      if (color === "red") {
+        cubeSet.red = Math.max(cubeSet.red, parseInt(num))
+      }
+      if (color === "green") {
+        cubeSet.green = Math.max(cubeSet.green, parseInt(num))
+      }
+      if (color === "blue") {
+        cubeSet.blue = Math.max(cubeSet.blue, parseInt(num))
+      }
+    }
+  }
+  return cubeSet
+}
+
+const allGames: [number, CubeSet][] = []
+let i = 1
+for (const game of getGames(__dirname + '/input')) {
+  const cubeSet = getCubes(game.replace(/Game \d+: /, ''))
+  allGames.push([i, cubeSet])
+  i++
+}
+
+let sum = 0
+for (const [gameNumber, game] of allGames) {
+  sum += game.red * game.green * game.blue
+}
+
+console.log(allGames)
+console.log('result:', sum)
+
